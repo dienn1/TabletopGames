928fd342e4719036eb57af9ac00b4d463728e3c2
dienn1 <dienn1@uci.edu>
2025-03-07 01:24:39 +0000

    Update DBGameStateContainer.java

--- src/main/java/games/dotsboxes/DBGameStateContainer.java
+++ src/main/java/games/dotsboxes/DBGameStateContainer.java
@@ -7,19 +7,12 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class DBGameStateContainer extends AbstractGameStateContainer {
-    // List of all edges possible
-    HashSet<DBEdge> edges;
-    // List of all cells possible
-    HashSet<DBCell> cells;
-
     // Mutable state:
     int[] nCellsPerPlayer;
     HashMap<DBCell, Integer> cellToOwnerMap;  // Mapping from each cell to its owner, if complete
     HashMap<DBEdge, Integer> edgeToOwnerMap;  // Mapping from each edge to its owner, if placed
     protected DBGameStateContainer(DBGameState gs) {
         super(gs);
-        edges = gs.edges;
-        cells = gs.cells;
         nCellsPerPlayer = gs.nCellsPerPlayer;
         cellToOwnerMap = gs.cellToOwnerMap;  // Mapping from each cell to its owner, if complete
         edgeToOwnerMap = gs.edgeToOwnerMap;  // Mapping from each edge to its owner, if placed
