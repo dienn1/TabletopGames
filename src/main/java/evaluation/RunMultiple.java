0d97818cd1650ece9a63acf6bc9a24728e6e1b29
dienn1 <dienn1@uci.edu>
2025-03-07 04:18:23 +0000

    Update RunMultiple.java

--- src/main/java/evaluation/RunMultiple.java
+++ src/main/java/evaluation/RunMultiple.java
@@ -2,6 +2,7 @@ package evaluation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class RunMultiple {
     // TODO parse from args
@@ -9,21 +10,24 @@ public class RunMultiple {
     private static List<String> getConfigs(String gameName, String[] agentTypes, int paramCount, int seedCount) {
         String dir = "Experiments/" + gameName;
         return new ArrayList<>(){{
-//            for (String agent : agentTypes) {
-//                add(dir + "/agents/" + gameName.toLowerCase() + "Run" + agent + ".json");
-//            }
-//            for (int i = 1; i <= paramCount; i++) {
-//                add(dir + "/gameParams/" + gameName.toLowerCase() + "Run" + "Param" + i + ".json");
-//            }
-            for (int i = 1; i <= seedCount; i++) {
-                add(dir + "/seeds/" + gameName.toLowerCase() + "Run" + "Seed" + i + ".json");
+            for (String agent : agentTypes) {
+                add(dir + "/agents/" + gameName.toLowerCase() + "Run" + agent + ".json");
+            }
+            for (int i = 1; i <= paramCount; i++) {
+                add(dir + "/gameParams/" + gameName.toLowerCase() + "Run" + "Param" + i + ".json");
+            }
+            if (!gameName.equals("DotsAndBoxes") && !gameName.equals("Connect4"))
+            {
+                for (int i = 1; i <= seedCount; i++) {
+                    add(dir + "/seeds/" + gameName.toLowerCase() + "Run" + "Seed" + i + ".json");
+                }
             }
         }};
     }
 
     public static void main(String[] args) {
         List<String> gameNames = new ArrayList<>(){{
-//            add("DotsAndBoxes");
+            add("DotsAndBoxes");
             add("CantStop");
         }};
         String[] agentTypes = new String[] {
@@ -39,6 +43,7 @@ public class RunMultiple {
         for (String name : gameNames) {
             configs.addAll(getConfigs(name, agentTypes, paramCount, seedCount));
         }
+//        System.out.println(configs);
         long t = System.currentTimeMillis();
         for (String configPath : configs) {
             System.out.println("Running " + configPath);
